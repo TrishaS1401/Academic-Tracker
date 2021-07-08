88fc88e0b63374539acf6277e7af0a592fc5afe8
Trisha Shetty <trishashetty01@gmail.com>
2021-07-08 13:06:17 +0530

    first commit

---
+++ Academic_Tracker/accounts/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from .models import student, subject, attendance, StudentsR, AdminR, adminsubject
+
+
+
+admin.site.register(StudentsR)
+admin.site.register(AdminR)
+admin.site.register(student)
+admin.site.register(subject)
+admin.site.register(adminsubject)
+admin.site.register(attendance)
+
+# Register your models here.
